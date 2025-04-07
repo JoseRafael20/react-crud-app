5eeee909bfcdcd528cdcdda9bc077e304dc7b677
JoseRafael20 <jgarado@paterostechnologicalcollege.edu.ph>
2025-04-07 19:39:38 +0800

    Initial commit of React CRUD project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//src/App.js
+import React from 'react';
+import List from './components/List';
+
+function App() {
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
+}
+
+export default App;
